72b0eb9cd0b83fd78cca5c87ce90d7af2ccc8367
EC2 Default User <ec2-user@ip-172-31-84-127.ec2.internal>
2020-02-06 21:06:17 +0000

    part 1 and part of part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,10 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.ratings
     @movies = Movie.all
+    @movies = Movie.order(params[:sort_by])
+    @sort_colum = params[:sort_by]
   end
 
   def new
