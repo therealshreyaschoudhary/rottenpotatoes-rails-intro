a4a26caff1527f0e4f90709d37ff94e9cf06b653
EC2 Default User <ec2-user@ip-172-31-84-127.ec2.internal>
2020-02-07 19:09:00 +0000

    part 3 error update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,19 +13,21 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.ratings
     
-    @sorted_column = params[:sort_by] || session[:sort]
+    session[:sort] = params[:sort_by] || session[:sort]
     
-    session[:ratings] = session[:ratings] || @all_ratings
-    
-    @rating_param = params[:ratings] || session[:ratings]
-    
-    session[:sort] = @sorted_column
-    
-    session[:ratings] = @rating_param
-    
+    session[:ratings] = params[:ratings] || session[:ratings]
+    @rating_param = session[:ratings]
+
+
+
     
     #params[:ratings].nil? ? rating_param = @all_ratings : rating_param  = params[:ratings].keys
-    @movies = Movie.where(rating:session[:ratings].keys).order(session[:sort])
+    if params[:ratings].nil? and session[:ratings].nil? 
+      session[:ratings] = @all_ratings
+      @movies = Movie.where(rating:session[:ratings].keys).order(session[:sort])
+    else
+      @movies = Movie.where(rating:session[:ratings]).order(session[:sort])
+    end
     
     if (params[:sort_by].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
       flash.keep
